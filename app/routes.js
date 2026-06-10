7bb83c60706c28fa839342fa8726458af0210a75
hollyyelland <holly.yelland@education.gov.uk>
2026-06-10 10:26:12 +0100

    init commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,17 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.get('/apprenticeship-units/apprenticeship-unit-list', function (req, res) {
+
+  // Store current banner state
+  const showBanner = req.session.data['showBanner']
+
+  // Immediately reset it
+  req.session.data['showBanner'] = false
+
+  // Render page with the value
+  res.render('apprenticeship-units/apprenticeship-unit-list', {
+    showBanner: showBanner
+  })
+})
